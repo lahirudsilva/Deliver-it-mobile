75cc51dcc6d4090938b1f222decde990d8c530b4
lahiru de silva <desilvalahiru4@gmail.com>
2021-11-27 21:13:14 +0530

    first commit

---
+++ app/src/main/java/com/typical_coderr/deliverit_mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.typical_coderr.deliverit_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
