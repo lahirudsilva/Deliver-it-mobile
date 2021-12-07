64fa93bd08f26be9bcfbae768c3cbd993aece79d
lahiru de silva <desilvalahiru4@gmail.com>
2021-12-07 17:54:10 +0530

    [+] customer past rides

--- app/src/main/java/com/typical_coderr/deliverit_mobile/CustomerDeliveryHistoryActivity.java
+++ app/src/main/java/com/typical_coderr/deliverit_mobile/CustomerDeliveryHistoryActivity.java
@@ -1,26 +1,20 @@
 package com.typical_coderr.deliverit_mobile;
 
 import android.app.ProgressDialog;
-import android.app.SearchManager;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,10 +22,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.navigation.NavigationView;
 import com.typical_coderr.deliverit_mobile.adapter.PastRidesAdapter;
 import com.typical_coderr.deliverit_mobile.model.Shipment;
+import com.typical_coderr.deliverit_mobile.model.User;
 import com.typical_coderr.deliverit_mobile.service.RetrofitClientInstance;
 import com.typical_coderr.deliverit_mobile.service.ShipmentClient;
 import com.typical_coderr.deliverit_mobile.util.AuthHandler;
-import com.typical_coderr.deliverit_mobile.util.NavHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,10 +37,10 @@ import retrofit2.Response;
 /**
  * Created by Android Studio.
  * User: Lahiru
- * Date: Sat
- * Time: 7:28 PM
+ * Date: Mon
+ * Time: 12:10 PM
  */
-public class PastRidesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class CustomerDeliveryHistoryActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
@@ -58,19 +52,21 @@ public class PastRidesActivity extends AppCompatActivity implements NavigationVi
     private RecyclerView recyclerView;
     private PastRidesAdapter pastRidesAdapter;
     private String jwtToken;
+    private User user;
+
     private List<Shipment> pastShipment;
     private boolean resultsRetrieved;
 
-    private ShipmentClient shipmentClient = RetrofitClientInstance.getRetrofitInstance().create(ShipmentClient.class);
 
+    private ShipmentClient shipmentClient = RetrofitClientInstance.getRetrofitInstance().create(ShipmentClient.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_past_rides);
+        setContentView(R.layout.activity_customer_history);
 
         //Check if authorization token is valid
-        AuthHandler.validate(PastRidesActivity.this, "driver");
+        AuthHandler.validate(CustomerDeliveryHistoryActivity.this, "customer");
 
         //Retrieve JWT Token
         SharedPreferences sharedPreferences = getSharedPreferences("auth_preferences", Context.MODE_PRIVATE);
@@ -87,20 +83,20 @@ public class PastRidesActivity extends AppCompatActivity implements NavigationVi
         recyclerView = findViewById(R.id.pastRides_recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        pastRidesAdapter = new PastRidesAdapter(this, pastShipment, "driver", jwtToken, mProgressDialog);
+        pastRidesAdapter = new PastRidesAdapter(this, pastShipment, "customer", jwtToken, mProgressDialog);
         recyclerView.setAdapter(pastRidesAdapter);
 
         mEmptyView = findViewById(R.id.empty_pick_view);
 
-        getAllPastRide();
-
+        getAllCustomerHistory();
     }
 
-    private void getAllPastRide() {
-        Call<List<Shipment>> call = shipmentClient.getAllPastDeliveries(jwtToken);
+    private void getAllCustomerHistory() {
+
+        Call<List<Shipment>> call = shipmentClient.getCustomerShipmentHistory(jwtToken);
 
         //Show progress
-        mProgressDialog.setMessage("Loading past rides...");
+        mProgressDialog.setMessage("Loading past packages...");
         mProgressDialog.show();
 
         call.enqueue(new Callback<List<Shipment>>() {
@@ -115,15 +111,16 @@ public class PastRidesActivity extends AppCompatActivity implements NavigationVi
                     mEmptyView.setVisibility(pastShipment.isEmpty() ? View.VISIBLE : View.GONE);
 
                 } else {
-                    Toast.makeText(PastRidesActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(CustomerDeliveryHistoryActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
 
                 }
                 mProgressDialog.dismiss();
+
             }
 
             @Override
             public void onFailure(Call<List<Shipment>> call, Throwable t) {
-                Toast.makeText(PastRidesActivity.this, "Something went wrong!" + t.toString(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(CustomerDeliveryHistoryActivity.this, "Something went wrong!" + t.toString(), Toast.LENGTH_SHORT).show();
                 mProgressDialog.dismiss();
             }
         });
@@ -131,51 +128,11 @@ public class PastRidesActivity extends AppCompatActivity implements NavigationVi
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        NavHandler.handleDriverNav(item, PastRidesActivity.this);
-
-        //close navigation drawer
-        mDrawerLayout.closeDrawer(GravityCompat.START);
-        return true;
+        return false;
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        // Not calling **super**, disables back button in current screen.
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.options_menu, menu);
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setMaxWidth(Integer.MAX_VALUE);
-
-        //changing search view text color
-        int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
-        TextView textView = (TextView) searchView.findViewById(id);
-        textView.setTextColor(Color.WHITE);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                pastRidesAdapter.getFilter().filter(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String query) {
-                pastRidesAdapter.getFilter().filter(query);
-                return false;
-            }
-        });
-        return true;
 
     }
 }
