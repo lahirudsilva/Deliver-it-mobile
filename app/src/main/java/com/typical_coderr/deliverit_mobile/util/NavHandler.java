3548fec88e415acb98749aef1a2ce196ac2ff967
lahiru de silva <desilvalahiru4@gmail.com>
2021-12-20 22:52:32 +0530

    [+] for supervisor

--- app/src/main/java/com/typical_coderr/deliverit_mobile/util/NavHandler.java
+++ app/src/main/java/com/typical_coderr/deliverit_mobile/util/NavHandler.java
@@ -8,8 +8,12 @@ import com.typical_coderr.deliverit_mobile.CreatePackageActivity;
 import com.typical_coderr.deliverit_mobile.CustomerDeliveryHistoryActivity;
 import com.typical_coderr.deliverit_mobile.CustomerHomeActivity;
 import com.typical_coderr.deliverit_mobile.DriverHomeActivity;
+import com.typical_coderr.deliverit_mobile.InquiriesActivity;
 import com.typical_coderr.deliverit_mobile.ManageDeliveryRidesActivity;
+import com.typical_coderr.deliverit_mobile.OnGoingShipmentsActivity;
+import com.typical_coderr.deliverit_mobile.PackageRequestsActivity;
 import com.typical_coderr.deliverit_mobile.R;
+import com.typical_coderr.deliverit_mobile.SupervisorHomeActivity;
 import com.typical_coderr.deliverit_mobile.UserProfileActivity;
 
 public class NavHandler {
@@ -73,6 +77,40 @@ public class NavHandler {
             }
         }
 
+
+    }
+
+    public static void handleSupervisorNav(MenuItem item, Context context){
+        switch (item.getItemId()){
+            case R.id.nav_home: {
+                Intent intent = new Intent(context, SupervisorHomeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                context.startActivity(intent);
+                break;
+            }
+            case R.id.nav_package_requests: {
+                Intent intent = new Intent(context, PackageRequestsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                context.startActivity(intent);
+                break;
+            }
+            case R.id.nav_inquires: {
+                Intent intent = new Intent(context, InquiriesActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                context.startActivity(intent);
+                break;
+            }
+            case R.id.nav_on_going_shipment: {
+                Intent intent = new Intent(context, OnGoingShipmentsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                context.startActivity(intent);
+                break;
+            }
+            case R.id.nav_logout: {
+                AuthHandler.logout(context);
+                break;
+            }
+        }
 
     }
 }
