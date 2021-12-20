cc8a7336b0bbbdd41efc65db0c14aa0d06e1a995
lahiru de silva <desilvalahiru4@gmail.com>
2021-12-20 22:48:13 +0530

    [implemented] package requests

---
+++ app/src/main/java/com/typical_coderr/deliverit_mobile/PackageRequests.java
@@ -0,0 +1,65 @@
+package com.typical_coderr.deliverit_mobile;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.typical_coderr.deliverit_mobile.util.AuthHandler;
+
+/**
+ * Created by Android Studio.
+ * User: Lahiru
+ * Date: Mon
+ * Time: 5:22 PM
+ */
+public class PackageRequests extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ProgressDialog mProgressDialog;
+    private NavigationView mNavigationView;
+
+    private String jwtToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_requests);
+
+        //Check if authorization token is valid
+        AuthHandler.validate(PackageRequests.this, "supervisor");
+
+        //Retrieve JWT Token
+        SharedPreferences sharedPreferences = getSharedPreferences("auth_preferences", Context.MODE_PRIVATE);
+        jwtToken = "Bearer " + sharedPreferences.getString("auth_token", null);
+
+        //Setup toolbar
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mProgressDialog = new ProgressDialog(this);
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
